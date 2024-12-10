a61f1e24a55590c3279dfe42631515407c0e0df1
James Ferrier <rjferrier@protonmail.com>
2024-12-10 17:40:49 +0000

    86c18c3jb Implement hook_civicrm_enable
    This hook will intercept any contribution creations that are associated with
    the 'Gift' financial type. Note that this financial type is not native to
    CiviCRM; it needs to be created.
    
    For every such contribution, the hook will result in a negative sign on each
    of the following entries:
    
    * `amount` in `civicrm_financial_item`
    * `unit_price` and `line_total` in `civicrm_line_item`
    * `total_amount` and `net_amount` in `civicrm_financial_trxn`
    
    It is assumed that fees and taxes for these contributions are zero.

--- kincoop.php
+++ kincoop.php
@@ -4,6 +4,27 @@ require_once 'kincoop.civix.php';
 
 use CRM_Kincoop_ExtensionUtil as E;
 
+const GIFT_FT_NAME = 'Gift';
+
+const REVERSIBLE_AMOUNT_KEYS = array(
+  'line_total' => TRUE,
+  'line_total_inclusive' => TRUE,
+  'net_amount' => TRUE,
+  'total_amount' => TRUE,
+  'unit_price' => TRUE,
+);
+
+/**
+ * Implements hook_civicrm_pre
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_pre
+ */
+function kincoop_civicrm_pre($op, $objectName, $id, &$params) {
+  if (isGiftRequest($op, $objectName, $params['financial_type_id'])) {
+    array_walk_recursive($params, 'reverse_sign_if_appropriate');
+  }
+}
+
 /**
  * Implements hook_civicrm_config().
  *
@@ -29,4 +50,30 @@ function kincoop_civicrm_install(): void {
  */
 function kincoop_civicrm_enable(): void {
   _kincoop_civix_civicrm_enable();
+}
+
+function isGiftRequest($op, $objectName, $financialTypeId): bool {
+  return $objectName == 'Contribution' && $op == 'create' && isAssociatedWithGift($financialTypeId);
+}
+
+function isAssociatedWithGift($financial_type_id): bool {
+  $ftName = CRM_Core_DAO::singleValueQuery('SELECT name FROM civicrm_financial_type WHERE id = %1',
+    array(1 => array($financial_type_id, 'Integer')));
+  return $ftName == GIFT_FT_NAME;
+}
+
+function reverse_sign_if_appropriate(&$item, $key): void {
+  if (!isReversibleAmount($key)) {
+    return;
+  }
+  if (is_numeric($item)) {
+    $item = -$item;
+  } elseif (is_string($item)) {
+    $item = '-' . $item;
+  }
+}
+
+function isReversibleAmount($key): bool
+{
+  return array_key_exists($key, REVERSIBLE_AMOUNT_KEYS);
 }
